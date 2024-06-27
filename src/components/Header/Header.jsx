d53b6c110579ee9da7db94db4da10756fea36d55
Byteers <131696489+Byteers@users.noreply.github.com>
2024-06-27 18:53:16 +0530

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { signOutAPI, changeDayNight } from '../../redux/actions';
-import './Header.scss';
-import { Link } from 'react-router-dom';
-import DarkLightSwitch from '../DarkLightSwitch/DarkLightSwitch';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { signOutAPI, changeDayNight } from "../../redux/actions";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import DarkLightSwitch from "../DarkLightSwitch/DarkLightSwitch";
 const Header = () => {
   const [fullscreen, setFullscreen] = useState(false);
   const data = useSelector((state) => state.userState);
@@ -36,45 +36,45 @@ const Header = () => {
     }
   };
   return (
-    <nav className='wrap'>
-      <Link to='/'>
-        <img src='/assets/icons/lofi-logo.gif' alt='' />
+    <nav className="wrap">
+      <Link to="/">
+        <img src="/assets/icons/lofi-logo.gif" alt="" />
       </Link>
-      <div className='nav-menu'>
-        <Link to='/about'>
-          <i className='fas fa-info'></i>
+      <div className="nav-menu">
+        <Link to="/about">
+          <i className="fas fa-info"></i>
           <span>How it works</span>
         </Link>
         <a
-          target='_blank'
-          rel='noreferrer'
-          href='https://github.com/Ken0803/Lofi-website'
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/Byteers/Lofi-Env-Study.git"
         >
-          <i className='fab fa-github'></i>
+          <i className="fab fa-github"></i>
           <span>GitHub</span>
         </a>
       </div>
-      <div className='nav-menu'>
+      <div className="nav-menu">
         <div onClick={daynightHandler}>
           <DarkLightSwitch theme={mode} />
         </div>
-        <button onClick={fullscreenHandler} className='fullscreen-btn'>
-          <i className='fas fa-expand fa-lg'></i>
+        <button onClick={fullscreenHandler} className="fullscreen-btn">
+          <i className="fas fa-expand fa-lg"></i>
         </button>
       </div>
-      <div className='nav-menu'>
-        <Link to='/user'>
-          {user && user.photoURL && <img src={user.photoURL} alt='' />}
+      <div className="nav-menu">
+        <Link to="/user">
+          {user && user.photoURL && <img src={user.photoURL} alt="" />}
           {user && user.displayName && <span>{user.displayName}</span>}
         </Link>
         {user ? (
-          <Link to='/' onClick={signOutHandler}>
-            <i className='fas fa-sign-out-alt'></i>
+          <Link to="/" onClick={signOutHandler}>
+            <i className="fas fa-sign-out-alt"></i>
             <span>Log Out</span>
           </Link>
         ) : (
-          <Link to='/login'>
-            <i className='fas fa-sign-in-alt'></i>
+          <Link to="/login">
+            <i className="fas fa-sign-in-alt"></i>
             <span>Log In</span>
           </Link>
         )}
